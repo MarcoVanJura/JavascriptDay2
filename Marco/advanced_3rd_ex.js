7172e62dbed624cf8e2c31474aab46aef5ed9596
MarcoVanJura <112858300+MarcoVanJura@users.noreply.github.com>
2022-09-22 00:37:24 +0200

    finished challenge

---
+++ Marco/advanced_3rd_ex.js
@@ -0,0 +1,15 @@
+let number = [
+        [1, 2, 1, 24],
+        [8, 11, 9, 4],
+        [7, 0, 7, 27],
+        [7, 4, 28, 14],
+        [3, 10, 25, 7],
+        [21, 4, 6, 17],
+        [3, 5, 26, 3]
+    ]
+    // Select and output the numbers 11, 25, 17, 27 and 0 in the console.
+console.log(number)
+console.log(number[1][1], number[4][2], number[5][3], number[2][3], number[2][1])
+let number_2 = []
+number_2.push(number[1][1], number[4][2], number[5][3], number[2][3], number[2][1])
+console.log(number_2)
