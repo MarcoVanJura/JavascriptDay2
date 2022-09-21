53a653f937fd442a21087a59aa834a3862486b94
Yevgeniy Kolesnik <yevgeniy.kolesnik@gmail.com>
2022-09-21 15:25:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+console.log("Hello from the Console!");
+var user_name = "Hi my name is Martin";
+console.log(`${user_name}`);
+var user_age = " and I am 25 years old!";
+console.log(`${user_age}`);
+
+const team = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+console.log(team);
+
+var players = "The most valuable player of the match is  ";
+console.table(team);
+document.write("The most valuable player of the match is " + team [2] + ".");
